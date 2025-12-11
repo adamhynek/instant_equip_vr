939a1561b1da8701074b9d0e3d9a2549bbc286e0
Adam Hynek <adamhynek@gmail.com>
2025-12-10 21:16:13 -0800

    Use cleaner hook and add configuration

---
+++ include/config.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "skse64/NiNodes.h"
+#include "skse64/GameData.h"
+
+
+namespace Config {
+    struct Options {
+        int numSkipAnimationFrames = 1;
+        float skipAnimationDeltaTime = 10000.f;
+    };
+    extern Options options; // global object containing options
+
+
+    // Fills Options struct from INI file
+    bool ReadConfigOptions();
+
+    const std::string & GetConfigPath();
+
+    std::string GetConfigOption(const char * section, const char * key);
+
+    bool GetConfigOptionDouble(const char *section, const char *key, double *out);
+    bool GetConfigOptionFloat(const char *section, const char *key, float *out);
+    bool GetConfigOptionInt(const char *section, const char *key, int *out);
+    bool GetConfigOptionBool(const char *section, const char *key, bool *out);
+}
