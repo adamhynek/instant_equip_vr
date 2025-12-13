d920b19f771edc0a69c16627e72575bbca318fcb
Adam Hynek <adamhynek@gmail.com>
2025-12-13 15:32:43 -0800

    Revert to original delta time for weapon animations

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "skse64/GameData.h"
 #include "skse64_common/SafeWrite.h"
 #include "skse64/GameRTTI.h"
+#include "skse64_common/BranchTrampoline.h"
 
 #include <ShlObj.h>  // CSIDL_MYDOCUMENTS
 #include "version.h"
@@ -13,6 +14,7 @@
 // SKSE globals
 static PluginHandle	g_pluginHandle = kPluginHandle_Invalid;
 static SKSEMessagingInterface *g_messaging = nullptr;
+SKSETrampolineInterface *g_trampoline = nullptr;
 
 int g_numSkipAnimationFrames = 0;
 
@@ -70,17 +72,115 @@ struct EquipActionEventHandler : ActionEventHandler
 EquipActionEventHandler equipActionHandler;
 
 
+float g_originalDeltaTime = 0.0f;
+
 // PlayerCharacter::UpdateAnimation hook for altering the player's animation rate
 typedef void(*_Actor_UpdateAnimation)(Actor *_this, float deltaTime);
 _Actor_UpdateAnimation g_originalPCUpdateAnimation = nullptr;
 static RelocPtr<_Actor_UpdateAnimation> PlayerCharacter_UpdateAnimation_vtbl(0x16E2618); // 0x16E2230 + 0x7D * 8
 void PlayerCharacter_UpdateAnimation_Hook(Actor *_this, float deltaTime)
 {
+    g_originalDeltaTime = deltaTime;
+
     if (g_numSkipAnimationFrames > 0) {
         --g_numSkipAnimationFrames;
         deltaTime = Config::options.skipAnimationDeltaTime;
     }
     g_originalPCUpdateAnimation(_this, deltaTime);
+}
+
+
+struct BShkbAnimationGraph_UpdateData
+{
+        float deltaTime; // 00
+        void *unkFunctionPtr08; // function pointer to some Character function
+        Character *character; // 10
+        NiPoint3 *cameraPos; // 18 - literally points to the pos within the worldTransform of the WorldRoot NiCamera node
+        IPostAnimationChannelUpdateFunctor *unk20; // points to the IPostAnimationChannelUpdateFunctor part of the Character
+        UInt8 unk28;
+        UInt8 unk29;
+        UInt8 forceUpdate; // 2A
+        UInt8 unk2B;
+        UInt8 unk2C;
+        UInt8 useGenerateJob; // 2D - if 0, call generate(). If not 0, queue up a job for it
+        UInt8 doFootIK; // 2E
+        UInt8 unk2F;
+        float scale1; // 30 - for rabbit, 1.3f
+        float scale2; // 34 - for rabbit, 1.3f
+        // ...
+};
+
+typedef bool(*_PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects)(PlayerCharacter *player, void **bipedAnim, BShkbAnimationGraph_UpdateData *updateData);
+_PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects g_originalPCUpdateWeaponAnimationOnAllBipedObjects3p = nullptr;
+_PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects g_originalPCUpdateWeaponAnimationOnAllBipedObjects1p = nullptr;
+static RelocPtr<_PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects> PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects_3p_HookLoc(0x6C574E);
+static RelocPtr<_PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects> PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects_1p_HookLoc(0x6C576B);
+
+bool PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects_3p_Hook(PlayerCharacter *player, void **bipedAnim, BShkbAnimationGraph_UpdateData *updateData)
+{
+    float overridenDeltaTime = updateData->deltaTime;
+    updateData->deltaTime = g_originalDeltaTime;
+    bool ret = g_originalPCUpdateWeaponAnimationOnAllBipedObjects3p(player, bipedAnim, updateData);
+    updateData->deltaTime = overridenDeltaTime;
+    return ret;
+}
+
+bool PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects_1p_Hook(PlayerCharacter *player, void **bipedAnim, BShkbAnimationGraph_UpdateData *updateData)
+{
+    float overridenDeltaTime = updateData->deltaTime;
+    updateData->deltaTime = g_originalDeltaTime;
+    bool ret = g_originalPCUpdateWeaponAnimationOnAllBipedObjects1p(player, bipedAnim, updateData);
+    updateData->deltaTime = overridenDeltaTime;
+    return ret;
+}
+
+
+bool TryHook()
+{
+    // This should be sized to the actual amount used by your trampoline
+    static const size_t TRAMPOLINE_SIZE = 32;
+
+    if (g_trampoline) {
+        void *branch = g_trampoline->AllocateFromBranchPool(g_pluginHandle, TRAMPOLINE_SIZE);
+        if (!branch) {
+            _ERROR("couldn't acquire branch trampoline from SKSE. this is fatal. skipping remainder of init process.");
+            return false;
+        }
+
+        g_branchTrampoline.SetBase(TRAMPOLINE_SIZE, branch);
+
+        void *local = g_trampoline->AllocateFromLocalPool(g_pluginHandle, TRAMPOLINE_SIZE);
+        if (!local) {
+            _ERROR("couldn't acquire codegen buffer from SKSE. this is fatal. skipping remainder of init process.");
+            return false;
+        }
+
+        g_localTrampoline.SetBase(TRAMPOLINE_SIZE, local);
+    }
+    else {
+        if (!g_branchTrampoline.Create(TRAMPOLINE_SIZE)) {
+            _ERROR("couldn't create branch trampoline. this is fatal. skipping remainder of init process.");
+            return false;
+        }
+        if (!g_localTrampoline.Create(TRAMPOLINE_SIZE, nullptr))
+        {
+            _ERROR("couldn't create codegen buffer. this is fatal. skipping remainder of init process.");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::uintptr_t Write5Call(std::uintptr_t a_src, std::uintptr_t a_dst)
+{
+    const auto disp = reinterpret_cast<std::int32_t *>(a_src + 1);
+    const auto nextOp = a_src + 5;
+    const auto func = nextOp + *disp;
+
+    g_branchTrampoline.Write5Call(a_src, a_dst);
+
+    return func;
 }
 
 
@@ -150,8 +250,26 @@ extern "C" {
         g_messaging = (SKSEMessagingInterface*)skse->QueryInterface(kInterface_Messaging);
         g_messaging->RegisterListener(g_pluginHandle, "SKSE", OnSKSEMessage);
 
-        g_originalPCUpdateAnimation = *PlayerCharacter_UpdateAnimation_vtbl;
-        SafeWrite64(PlayerCharacter_UpdateAnimation_vtbl.GetUIntPtr(), uintptr_t(PlayerCharacter_UpdateAnimation_Hook));
+        g_trampoline = (SKSETrampolineInterface *)skse->QueryInterface(kInterface_Trampoline);
+        if (!g_trampoline) {
+            _WARNING("Couldn't get trampoline interface");
+        }
+        if (!TryHook()) {
+            _ERROR("[CRITICAL] Failed to perform hooks");
+            return false;
+        }
+
+        {
+            g_originalPCUpdateAnimation = *PlayerCharacter_UpdateAnimation_vtbl;
+            SafeWrite64(PlayerCharacter_UpdateAnimation_vtbl.GetUIntPtr(), uintptr_t(PlayerCharacter_UpdateAnimation_Hook));
+        }
+
+        {
+            std::uintptr_t originalFunc = Write5Call(PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects_3p_HookLoc.GetUIntPtr(), uintptr_t(PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects_3p_Hook));
+            g_originalPCUpdateWeaponAnimationOnAllBipedObjects3p = (_PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects)originalFunc;
+            originalFunc = Write5Call(PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects_1p_HookLoc.GetUIntPtr(), uintptr_t(PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects_1p_Hook));
+            g_originalPCUpdateWeaponAnimationOnAllBipedObjects1p = (_PlayerCharacter_UpdateWeaponAnimationOnAllBipedObjects)originalFunc;
+        }
 
         return true;
     }
